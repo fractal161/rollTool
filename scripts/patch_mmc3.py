581577e6a47afc50b02cf7a22fbc53de73b19e95
fractal161 <628snapdragon@gmail.com>
2023-06-06 17:10:17 -0500

    fix mirroring for everdrive compatibility

--- scripts/patch_mmc3.py
+++ scripts/patch_mmc3.py
@@ -1,11 +1,14 @@
+PRG_BANKS = 2
+CHR_BANKS = 2
+# header + prg + chr
+NEW_SIZE = 16 + 16384*PRG_BANKS + 8192*CHR_BANKS
+
 rom = list(open('roll.nes', 'rb').read())
-# set number of chr banks to 3
-rom[5] = 0x03
+# set number of chr banks to 2 (4 tilesets)
+rom[5] = CHR_BANKS
 # set mapper to mmc3
-rom[6] = 0x40
+rom[6] = 0x41
 rom[7] = 0x08
 
-# header + prg + chr
-NEW_SIZE = 16+16384*2+8192*3
 with open('roll.nes', 'wb') as f:
     f.write(bytearray(rom[:NEW_SIZE]))
