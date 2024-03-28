3379feeec5e8c7012c836b3773b92a6acdff8b72
fractal161 <628snapdragon@gmail.com>
2024-03-27 20:05:40 -0400

    fix for kryzsio
    hack submitted by zohass (thx!) to fix nesfab arranging functions in
    inconvenient places (likely due to other hacks). in theory this should
    be fixable using +static, but this is bugged on this build of nesfab and
    later builds all seem to have performance regressions.

--- scripts/patch_mmc3.py
+++ scripts/patch_mmc3.py
@@ -10,5 +10,29 @@ rom[5] = CHR_BANKS
 rom[6] = 0x41
 rom[7] = 0x08
 
+# dummy reset patch (credits: zohassadar)
+patch = bytearray([0xa2,0x06,0xa0,0x00,0x8e,0x00,0x80,0x8c,0x01,0x80,0xe8,0xc8,0x8e,0x00,0x80,0x8c,0x01,0x80,0x4c])
+
+"""
+    ldx #$06
+    ldy #$00
+    stx $8000
+    sty $8001
+    inx
+    iny
+    stx $8000
+    sty $8001
+    jmp $8161
+"""
+
+reset_lo = rom[0x800c]
+reset_hi = rom[0x800d]
+
+patch.extend([reset_lo, reset_hi])
+
+rom[0x7f10:0x7f10+len(patch)] = patch
+rom[0x800c] = 0x00
+rom[0x800d] = 0xFF
+
 with open('roll.nes', 'wb') as f:
     f.write(bytearray(rom[:NEW_SIZE]))
